1ee1f84cbe27f28ef1338e97cdaca718a43f3a40
Landarak <davidalfonsosantiago@gmail.com>
2024-09-27 10:44:58 -0500

    Taller6
    Taller completo, se agrego lo correspondiente a zonas

---
+++ Taller 6/Taller 6/src/main/java/Zoo_Fantastico/app/SpringBootAppIntelljApplication.java
@@ -0,0 +1,18 @@
+package Zoo_Fantastico.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppIntelljApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppIntelljApplication.class, args);
+
+
+
+
+	}
+
+
+}
